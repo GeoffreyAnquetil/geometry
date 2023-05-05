e704c971045418236a8490023ef9305553777e00
GeoffreyAnquetil <geoffrey.anquetil1@gmail.com>
2023-05-05 11:07:43 +0200

    Rectification : ajout des setters et getters qui étaient absents au commit précédent

--- src/Segment2D.java
+++ src/Segment2D.java
@@ -10,6 +10,19 @@ public class Segment2D {
         }
     }
 
+    public Point2D getPoint1() {
+        return point1;
+    }
 
+    public Point2D getPoint2() {
+        return point2;
+    }
 
+    public void setPoint1(Point2D point1) {
+        this.point1 = point1;
+    }
+
+    public void setPoint2(Point2D point2) {
+        this.point2 = point2;
+    }
 }
