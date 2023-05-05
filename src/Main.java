fc12387c946d2a40f732c767f2ab576d24b241a1
GeoffreyAnquetil <geoffrey.anquetil1@gmail.com>
2023-05-05 10:48:01 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D p1 = new Point2D(1,2);
+        String p1String = p1.toString();
+        System.out.println(p1String);
+    }
+}
+
---
+++ src/Point2D.java
@@ -0,0 +1,29 @@
+public class Point2D {
+    private float x;
+    private float y;
+
+    public Point2D(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString(){
+        return("Point de coordonnées : ("+x+","+y+")");
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+}
